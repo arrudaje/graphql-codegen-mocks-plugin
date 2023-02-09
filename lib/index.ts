42d8d370fa08717dc2918fed8c66ed89d4aba294
arrudaje <arrudaje@gmail.com>
2023-02-09 13:41:04 +0100

    feat: Branch off if file already exists

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,23 @@
 import type { CodegenPlugin } from '@graphql-codegen/plugin-helpers';
 import fs from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
 
 const config: CodegenPlugin = {
     async plugin(schema, documents, config, info) {
+    
         if (info?.outputFile) {
+            const filePath = path.join(process.cwd(), info.outputFile);
             let fileExists = true;
             try {
-                await fs.access(info.outputFile, fs.constants.F_OK);
-            } catch {
+                await fs.access(filePath, constants.F_OK);
+            } catch (err) {
                 fileExists = false;
             }
             
             if (fileExists) {
-                const output = await fs.readFile(info.outputFile, { encoding: 'utf-8' });
-                return output + 'hi.';
+                const output = await fs.readFile(filePath, { encoding: 'utf-8' });
+                return 'hi.' + output;
             }
             return JSON.stringify(documents);
         }
